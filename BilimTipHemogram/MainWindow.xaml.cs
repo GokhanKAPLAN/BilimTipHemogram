d36f34c95123b2e7e73d03de8201bb4748900665
Gokhan <mit_black@hotmail.com>
2020-11-06 15:32:55 +0300

    Gereksiz importlar kaldırıldı

--- BilimTipHemogram/MainWindow.xaml.cs
+++ BilimTipHemogram/MainWindow.xaml.cs
@@ -1,7 +1,5 @@
-﻿using BilimTipHemogram.Model.Context;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Windows;
